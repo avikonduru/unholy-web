400f1d03225ddaa56f16170e6ca98637d161999e
Avi Konduru <avikonduru@gmail.com>
2022-04-03 10:41:52 -0700

    design updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+// Bootstrap
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,12 +8,16 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
-import Nav from 'react-bootstrap/Nav';
+
+// react icons
+import { FaDiscord, FaTwitter } from "react-icons/fa";
+
+const iconPath = process.env.PUBLIC_URL + '/assets/';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark" sticky="top">
+      <Navbar sticky="top" style={{backgroundColor: '#1d1f21'}}>
         <Container>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
@@ -23,19 +28,26 @@ function App() {
         </Container>
       </Navbar>
       <Container>
-        <Row style={{marginTop: 100, marginBottom: 20}}>
-          <Col xs={3}>
-            <span style={{fontWeight: 'bold', fontSize: 70}}>UnHoly</span>
+        <Row style={{marginTop: 60, marginBottom: 20}}>
+          <Col xs={5}>
+            <Image src={`${iconPath}unholy-logo-white-1080x1080.png`} width={500} />
           </Col>
           <Col style={{display: 'flex', alignItems: 'center'}}>
-            <span>Discord Twitter</span>
+            <Row>
+              <Col>
+                <FaDiscord style={{fontSize: 40}} />
+              </Col>
+              <Col>
+                <FaTwitter style={{fontSize: 40}} />
+              </Col>
+            </Row>
           </Col>
         </Row>
 
-        <Row style={{marginBottom: 20}}>
-          <Col xs={4}>
+        <Row style={{marginBottom: 20, marginLeft: 50}}>
+          <Col xs={5}>
             <Image
-              src="https://gateway.pinata.cloud/ipfs/QmdNF1aZ27XhE5SrFhCP4GtASLxbJfM6TNyhad4jKTpjxv/0.jpg"
+              src={`${iconPath}4_5929061502970497698.jpg`}
               width={400}
               style={{borderRadius: '20px'}}
             />
@@ -50,14 +62,14 @@ function App() {
                 <li>20 Acolyte NFT's left (Out of 400 in total)</li>
               </ul>
               <Row>
-                <Col xs={3}>
+                <Col xs={4}>
                   <div className="d-grid gap-2">
-                    <Button variant="primary">Mint Legendary</Button>
+                    <Button variant="primary" style={{fontWeight: 'bold'}}>MINT LEGENDARY</Button>
                   </div>
                 </Col>
-                <Col xs={3}>
+                <Col xs={4}>
                   <div className="d-grid gap-2">
-                    <Button variant="danger">Mint Acolyte</Button>
+                    <Button variant="danger" style={{fontWeight: 'bold'}}>MINT ACOLYTE</Button>
                   </div>
                 </Col>
               </Row>
@@ -72,7 +84,7 @@ function App() {
           <Row style={{marginBottom: 40}}>
             <span style={{fontWeight: 'bold', fontSize: 25, marginBottom: 20}}>Current Legendary Mints:</span>
             <Col xs={6}>
-              <Card style={{ width: '26rem', backgroundColor: 'purple' }}>
+              <Card style={{ width: '26rem', backgroundColor: '#dd02dd' }}>
                 <Card.Img variant="top" src="https://gateway.pinata.cloud/ipfs/QmdNF1aZ27XhE5SrFhCP4GtASLxbJfM6TNyhad4jKTpjxv/0.jpg" />
                 <Card.Body>
                   <Card.Title>Legendary #1</Card.Title>
@@ -84,7 +96,7 @@ function App() {
             </Col>
 
             <Col xs={6}>
-              <Card style={{ width: '26rem', backgroundColor: 'purple' }}>
+              <Card style={{ width: '26rem', backgroundColor: '#dd02dd' }}>
                 <Card.Img variant="top" src="https://gateway.pinata.cloud/ipfs/QmdNF1aZ27XhE5SrFhCP4GtASLxbJfM6TNyhad4jKTpjxv/0.jpg" />
                 <Card.Body>
                   <Card.Title>Legendary #1</Card.Title>
@@ -138,7 +150,7 @@ function App() {
         </Row>
       </Container>
 
-      <Navbar bg="dark" variant="dark" position="bottom">
+      <Navbar position="bottom" style={{backgroundColor: '#1d1f21'}}>
         <Container style={{display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white', padding: 20}}>
           <span>Come and see</span>
         </Container>
